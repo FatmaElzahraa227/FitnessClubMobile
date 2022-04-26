41a9f2935841675a67c020010bb675566c6fb5b4
K1REBEL <kibraheem47@yahoo.com>
2022-04-27 02:32:39 +0300

    blablabla

--- app/src/main/java/com/example/fitnessclubmobile/MainActivity3.java
+++ app/src/main/java/com/example/fitnessclubmobile/MainActivity3.java
@@ -2,7 +2,10 @@ package com.example.fitnessclubmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -13,7 +16,8 @@ public class MainActivity3 extends AppCompatActivity {
     }
 
 
-
-
-
+    public void launchMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
