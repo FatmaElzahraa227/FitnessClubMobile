1be0954717f43a77b0ffad22ff8c70ea88e0074c
K1REBEL <kibraheem47@yahoo.com>
2022-04-27 02:45:31 +0300

    marwa

--- app/src/main/java/com/example/fitnessclubmobile/MainActivity.java
+++ app/src/main/java/com/example/fitnessclubmobile/MainActivity.java
@@ -3,15 +3,32 @@ package com.example.fitnessclubmobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private CustomReceiver mReceiver ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mReceiver= new CustomReceiver();
+
+        IntentFilter filter = new IntentFilter();
+
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+
+        this.registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 
     public void launchActivityFour(View view) {
--- app/src/main/java/com/example/fitnessclubmobile/MainActivity7.java
+++ app/src/main/java/com/example/fitnessclubmobile/MainActivity7.java
@@ -2,7 +2,9 @@ package com.example.fitnessclubmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity7 extends AppCompatActivity {
 
@@ -11,4 +13,20 @@ public class MainActivity7 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main7);
     }
+
+    public void launchActivityFour(View view) {
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+
+    public void launchActivityFive(View view) {
+        Intent intent = new Intent(this, MainActivity5.class);
+        startActivity(intent);
+    }
+
+    public void launchActivitySix(View view) {
+        Intent intent = new Intent(this, MainActivity6.class);
+        startActivity(intent);
+    }
+
 }
