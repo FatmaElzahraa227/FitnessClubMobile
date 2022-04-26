6c47864859e15bae138acc6e0f579bdfa0378fe3
FatmaElzahraa227 <70411867+FatmaElzahraa227@users.noreply.github.com>
2022-04-27 01:44:41 +0200

    fatma..

--- app/src/main/java/com/example/fitnessclubmobile/MainActivity8.java
+++ app/src/main/java/com/example/fitnessclubmobile/MainActivity8.java
@@ -2,7 +2,9 @@ package com.example.fitnessclubmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity8 extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity8 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
+    }
+
+    public void launchActivitySix(View view) {
+        Intent intent = new Intent(this, MainActivity6.class);
+        startActivity(intent);
+    }
+
+    public void launchActivityFour(View view) {
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
+    }
+
+    public void launchActivityFive(View view) {
+        Intent intent = new Intent(this, MainActivity5.class);
+        startActivity(intent);
     }
 }
